b7f1c303c201584d95b465e690456d45db01f3c1
black-maxs <dermasg.works@gmail.com>
2024-09-10 08:23:47 +0700

    update say hello with parameter name

--- say_hello.go
+++ say_hello.go
@@ -1,5 +1,5 @@
 package go_say_hello
 
-func SayHello() string {
-	return "Hello, Black-Maxs! Apa kabar anda"
+func SayHello(name string) string {
+	return "Hello, Black-Maxs! Apa kabar anda" + name
 }
